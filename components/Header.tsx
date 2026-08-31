752d0ddffa6a68ac3fca203db48111d59e889c68
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-08-31 16:19:17 +0500

    feat: Add Header component and implement it to base layout.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+import { NAV_LINKS } from "@/constants/constants";
+
+const Header = () => {
+  return (
+    <header className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+          <Link href="/" className="flex items-center gap-3">
+            <div className="text-2xl tracking-wide">E-Buy</div>
+          </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href={NAV_LINKS.HOME}>Home</Link>
+            <Link href={NAV_LINKS.CATALOGUE}>Catalogue</Link>
+            <Link href={NAV_LINKS.ABOUT}>About</Link>
+            <Link href={NAV_LINKS.CONTACTS}>Contacts</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ constants/constants.ts
@@ -0,0 +1,6 @@
+export enum NAV_LINKS {
+  HOME = "/",
+  CATALOGUE = "/catalogue",
+  ABOUT = "/about",
+  CONTACTS = "/contacts",
+}
