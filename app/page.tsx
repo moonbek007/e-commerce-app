138f12fc552b25070680f6acd5f1f55fbcdf24ec
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-01 19:21:44 +0500

    feat: Implement Featured Products section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Slider from "@/components/Slider";
+import ProductList from "@/components/ProductList";
+
+import { PRODUCT_LIST_TITLES } from "@/constants/constants";
 
 export default function Home() {
   return (
     <main>
       <Slider />
+      <ProductList title={PRODUCT_LIST_TITLES.FEATURED} />
     </main>
   );
 }
