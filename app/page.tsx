df6cdbda362dca7b3f110d182c2e9d5c5de74494
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-01 17:22:14 +0500

    feat: Add Slider component, corresponding constants and add image urls in next config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Slider from "@/components/Slider";
+
 export default function Home() {
   return (
-    <main className="flex flex-1 w-full items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
-      E-commerce App
+    <main>
+      <Slider />
     </main>
   );
 }
--- constants/constants.ts
+++ constants/constants.ts
@@ -13,3 +13,27 @@ export enum CATALOGUE_FILTERS {
 export enum FORM_FIELDS {
   PRODUCT_NAME = "product-name",
 }
+
+export const slides = [
+  {
+    id: 1,
+    title: "Summer Sale Collections",
+    description: "Up to 30% off!",
+    img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 2,
+    title: "Winter Sale Collections",
+    description: "Up to 70% off!",
+    img: "https://images.pexels.com/photos/3262937/pexels-photo-3262937.jpeg",
+    bg: "bg-gradient-to-r from-pink-50 to-blue-50",
+  },
+  {
+    id: 3,
+    title: "Spring Sale Collections",
+    description: "Up to 50% off!",
+    img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800",
+    bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+  },
+];
