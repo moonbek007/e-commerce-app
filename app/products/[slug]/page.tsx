243cd5f4f0eb16f472ab88c15e06238501f0297f
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-02 17:44:59 +0500

    feat: Add basic layout for product-page

---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,9 @@
+const ProductPage = async () => {
+  return (
+    <main className="flex flex-1 justify-center items-center">
+      <div>Product Page</div>
+    </main>
+  );
+};
+
+export default ProductPage;
