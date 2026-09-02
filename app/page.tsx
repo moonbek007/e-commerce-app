1327d0ccb458bfdcd7949a759aa6610494e8ffda
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-02 16:03:56 +0500

    feat: Implement CategoryList to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Slider from "@/components/Slider";
 import ProductList from "@/components/ProductList";
+import CategoryList from "@/components/CategoryList";
 
 import { PRODUCT_LIST_TITLES } from "@/constants/constants";
 
@@ -7,8 +8,9 @@ export default function Home() {
   return (
     <main>
       <Slider />
-      <ProductList title={PRODUCT_LIST_TITLES.FEATURED} />
       <ProductList title={PRODUCT_LIST_TITLES.NEW} />
+      <CategoryList />
+      <ProductList title={PRODUCT_LIST_TITLES.FEATURED} />
     </main>
   );
 }
