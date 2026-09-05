7be3c4aba93211c6760d5bc6eac2ce0559f3da4a
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-05 18:11:19 +0500

    feat: Implement CartOrders component to cart-page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,10 @@
+import CartOrders from "@/components/CartOrders";
+
 const CartPage = async () => {
   return (
-    <div className="flex flex-1 justify-center items-center">Cart Page</div>
+    <main className="flex flex-1 justify-center items-center gap-12 py-12 bg-white text-gray-800 font-sans antialiased">
+      <CartOrders />
+    </main>
   );
 };
 
