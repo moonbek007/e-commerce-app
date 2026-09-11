38f42f9d3aa3668b64d8b1df05045ef6bc510509
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-11 21:45:11 +0500

    feat: Implement CustomerDetails, DeliveryDetails and PaymentDetails into CartUserInfo

--- components/CartUserInfo.tsx
+++ components/CartUserInfo.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { useReducer } from "react";
+import Link from "next/link";
+
+import CustomerDetails from "@/components/cart-user-info/CustomerDetails";
+import DeliveryDetails from "@/components/cart-user-info/DeliveryDetails";
+import PaymentDetails from "@/components/cart-user-info/PaymentDetails";
 
 import {
   CartUserInfoContext,
@@ -8,7 +13,7 @@ import {
 } from "@/lib/context";
 import { cartUserInfoRecuder } from "@/lib/reducer";
 
-import { defaultCartUserInfo } from "@/constants/constants";
+import { defaultCartUserInfo, NAV_LINKS } from "@/constants/constants";
 
 const CartUserInfo = () => {
   const [cartUserInfo, dispatch] = useReducer(
@@ -17,10 +22,21 @@ const CartUserInfo = () => {
   );
 
   return (
-    <div className="w-full max-w-md p-6">
+    <div className="w-full max-w-md px-6">
       <CartUserInfoContext value={cartUserInfo}>
         <CartUserInfoDispatchContext value={dispatch}>
-          Cart User Info
+          <div className="flex justify-center gap-2 bg-gray-50 p-2 text-md mb-5 rounded-md">
+            <span className="text-[#666666]">Have an account? </span>
+            <Link
+              href={NAV_LINKS.LOGIN}
+              className="text-black font-medium hover:text-gray-700"
+            >
+              Log In
+            </Link>
+          </div>
+          <CustomerDetails />
+          <DeliveryDetails />
+          <PaymentDetails />
         </CartUserInfoDispatchContext>
       </CartUserInfoContext>
     </div>
