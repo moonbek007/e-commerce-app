9482116f961fba4c6b0088fd6f49fb7dc0cad733
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-15 20:24:28 +0500

    refactor: Refactor CartUserInfo and implement TermsAndConditions

--- components/CartUserInfo.tsx
+++ components/CartUserInfo.tsx
@@ -1,30 +1,21 @@
 "use client";
 
-import { JSX } from "react";
 import Link from "next/link";
 
 import CustomerDetails from "@/components/cart-user-info/CustomerDetails";
 import DeliveryDetails from "@/components/cart-user-info/DeliveryDetails";
 import PaymentDetails from "@/components/cart-user-info/PaymentDetails";
+import TermsAndConditions from "@/components/cart-user-info/TermsAndConditions";
 
 import { useCartUserInfoContext } from "@/hooks/useCartUserInfoContext";
 
 import { CART_USER_INFO_SECTIONS, NAV_LINKS } from "@/constants/constants";
 
-const cartUserInfoSections: Record<CART_USER_INFO_SECTIONS, () => JSX.Element> =
-  {
-    [CART_USER_INFO_SECTIONS.CUSTOMER_DETAILS]: CustomerDetails,
-    [CART_USER_INFO_SECTIONS.DELIVERY_DETAILS]: DeliveryDetails,
-    [CART_USER_INFO_SECTIONS.PAYMENT_DETAILS]: PaymentDetails,
-  };
-
 const CartUserInfo = () => {
   const { activeSection } = useCartUserInfoContext();
 
-  const ActiveSectionComponent = cartUserInfoSections[activeSection];
-
   return (
-    <div className="w-full max-w-md px-6">
+    <div className="w-full max-w-md px-6 flex flex-col gap-4">
       <div className="flex justify-center gap-2 bg-gray-50 p-2 text-md mb-5 rounded-md">
         <span className="text-[#666666]">Have an account? </span>
         <Link
@@ -34,7 +25,17 @@ const CartUserInfo = () => {
           Log In
         </Link>
       </div>
-      <ActiveSectionComponent />
+      <CustomerDetails />
+      <div className="h-0.5 bg-gray-200"> </div>
+      <DeliveryDetails />
+      <div className="h-0.5 bg-gray-200"> </div>
+      <PaymentDetails />
+      {activeSection === CART_USER_INFO_SECTIONS.TERMS_AND_CONDTIONS && (
+        <>
+          <div className="h-0.5 bg-gray-200"> </div>
+          <TermsAndConditions />
+        </>
+      )}
     </div>
   );
 };
