ad51c1ea6f0ab1bbda8548190ab255730db92b7d
moonbek007 <oybek4ik.mamatov.00@mail.ru>
2026-09-15 20:21:33 +0500

    feat: Implement logic for CustomerDetails when user clicks continue or wants to edit

--- components/cart-user-info/CustomerDetails.tsx
+++ components/cart-user-info/CustomerDetails.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/constants/constants";
 
 const CustomerDetails = () => {
-  const { customerDetails } = useCartUserInfoContext();
+  const { customerDetails, activeSection } = useCartUserInfoContext();
   const dispatch = useCartUserInfoDispatch()!;
 
   const [formData, setFormData] = useState({ ...customerDetails });
@@ -35,78 +35,115 @@ const CustomerDetails = () => {
     });
   };
 
+  const handleClickEdit = () => {
+    dispatch({
+      type: CART_USER_INFO_ACTION_TYPES.SECTIONS_CHANGE_SECTION,
+      payload: {
+        newSection: CART_USER_INFO_SECTIONS.CUSTOMER_DETAILS,
+      },
+    });
+  };
+
   return (
     <div className="w-full max-w-125 font-sans antialiased text-[#2D2D2D]">
-      <h2 className="text-[22px] font-semibold tracking-wide mb-6">
-        Customer details
-      </h2>
-
-      <form className="space-y-5" onSubmit={handleClickContinue}>
-        {customerDetailsFormDataRows.map((row) => {
-          const formField = row.items[0];
-          return !row.multipleItems ? (
-            <div key={formField.label}>
-              <label
-                htmlFor={formField.label}
-                className="block text-xs font-medium text-[#2D2D2D] mb-1.5"
-              >
-                {formField.isRequired
-                  ? `${formField.title} *`
-                  : `${formField.title}`}
-              </label>
-              <input
-                type={formField.inputType}
-                id={formField.label}
-                name={formField.label}
-                value={formData[formField.label]}
-                onChange={handleChange}
-                className={clsx(
-                  "w-full h-11 px-3 border border-[#CCCCCC] rounded-sm focus:outline-none focus:border-black transition-colors",
-                  {
-                    "text-sm":
-                      formField.label !== CUSTOMER_DETAILS_FIELDS.FIRST_NAME &&
-                      formField.label !== CUSTOMER_DETAILS_FIELDS.LAST_NAME,
-                  },
-                )}
-                required={formField.isRequired}
-              />
-            </div>
-          ) : (
-            <div className="flex gap-3" key={formField.label}>
-              {row.items.map((field) => {
-                return (
-                  <div key={field.label}>
-                    <label
-                      htmlFor={field.label}
-                      className="block text-xs font-medium text-[#2D2D2D] mb-1.5"
-                    >
-                      {`${field.title} ${field.isRequired ? "*" : ""}`}
-                    </label>
-                    <input
-                      type={field.inputType}
-                      id={field.label}
-                      name={field.label}
-                      value={formData[field.label]}
-                      onChange={handleChange}
-                      className="w-full h-11 px-3 border text-sm border-[#CCCCCC] rounded-sm focus:outline-none focus:border-black transition-colors"
-                      required={row.items[0].isRequired}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-
-        <div>
+      <div className="flex justify-between items-center mb-2">
+        <h2 className="text-[22px] font-semibold tracking-wide">
+          Customer details
+        </h2>
+        {activeSection !== CART_USER_INFO_SECTIONS.CUSTOMER_DETAILS && (
           <button
-            type="submit"
-            className="w-full bg-black text-white text-sm font-medium py-3 rounded hover:bg-gray-900 transition-colors cursor-pointer"
+            className="text-sm cursor-pointer underline"
+            onClick={handleClickEdit}
           >
-            Continue
+            Edit
           </button>
+        )}
+      </div>
+
+      {activeSection === CART_USER_INFO_SECTIONS.CUSTOMER_DETAILS ? (
+        <form className="space-y-5" onSubmit={handleClickContinue}>
+          {customerDetailsFormDataRows.map((row) => {
+            const formField = row.items[0];
+            return !row.multipleItems ? (
+              <div key={formField.label}>
+                <label
+                  htmlFor={formField.label}
+                  className="block text-xs font-medium text-[#2D2D2D] mb-1.5"
+                >
+                  {formField.isRequired
+                    ? `${formField.title} *`
+                    : `${formField.title}`}
+                </label>
+                <input
+                  type={formField.inputType}
+                  id={formField.label}
+                  name={formField.label}
+                  value={formData[formField.label]}
+                  onChange={handleChange}
+                  className={clsx(
+                    "w-full h-11 px-3 border border-[#CCCCCC] rounded-sm focus:outline-none focus:border-black transition-colors",
+                    {
+                      "text-sm":
+                        formField.label !==
+                          CUSTOMER_DETAILS_FIELDS.FIRST_NAME &&
+                        formField.label !== CUSTOMER_DETAILS_FIELDS.LAST_NAME,
+                    },
+                  )}
+                  required={formField.isRequired}
+                />
+              </div>
+            ) : (
+              <div className="flex gap-3" key={formField.label}>
+                {row.items.map((field) => {
+                  return (
+                    <div key={field.label}>
+                      <label
+                        htmlFor={field.label}
+                        className="block text-xs font-medium text-[#2D2D2D] mb-1.5"
+                      >
+                        {`${field.title} ${field.isRequired ? "*" : ""}`}
+                      </label>
+                      <input
+                        type={field.inputType}
+                        id={field.label}
+                        name={field.label}
+                        value={formData[field.label]}
+                        onChange={handleChange}
+                        className="w-full h-11 px-3 border text-sm border-[#CCCCCC] rounded-sm focus:outline-none focus:border-black transition-colors"
+                        required={row.items[0].isRequired}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+
+          {activeSection === CART_USER_INFO_SECTIONS.CUSTOMER_DETAILS && (
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-black text-white text-sm font-medium py-3 rounded hover:bg-gray-900 transition-colors cursor-pointer"
+              >
+                Continue
+              </button>
+            </div>
+          )}
+        </form>
+      ) : (
+        <div>
+          <div className="flex gap-1">
+            <p>{customerDetails.firstName}</p>
+            <p>{customerDetails.lastName}</p>
+          </div>
+          <p>{customerDetails.address}</p>
+          <div className="flex gap-1">
+            <p>{customerDetails.postCode}</p>
+            <p>{customerDetails.city && `${customerDetails.city},`}</p>
+            <p>{customerDetails.countryOrRegion}</p>
+          </div>
         </div>
-      </form>
+      )}
     </div>
   );
 };
